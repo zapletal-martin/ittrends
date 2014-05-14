e7abb161ff7561c9714718f79371a2eb71cb3a01
Zapletal-Martin <zapletal-martin@email.cz>
2014-05-15 00:13:40 +0200

    Amendments to config and startup scripts.

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #
-kafka_2.10-0.8.1/bin/zookeeper-server-stop.sh
-kafka_2.10-0.8.1/bin/kafka-server-stop.sh
+kafka_2.10-0.8.1.1/bin/zookeeper-server-stop.sh
+kafka_2.10-0.8.1.1/bin/kafka-server-stop.sh
