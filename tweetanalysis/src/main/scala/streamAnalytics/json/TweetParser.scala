7720d51427fa8b6992b833a8a1540552175f1eff
Zapletal-Martin <zapletal-martin@email.cz>
2014-05-22 01:10:21 +0200

    Fixed issue where user mentions were not extracted correctly.

--- tweetanalysis/src/main/scala/streamAnalytics/json/TweetParser.scala
+++ tweetanalysis/src/main/scala/streamAnalytics/json/TweetParser.scala
@@ -7,18 +7,17 @@ trait TweetParser {
   def getTweet(data: String): Tweet = {
     val json = Json.parse(data)
     val user = json \ "user"
-    val mentions = json \ "user_mentions"
+    val mentions = json \ "entities"\ "user_mentions"
 
-    def extractUser(userJson: JsValue) = User(
-      (user \ "id_str").as[String],
-      (user \ "name").as[String],
-      (user \ "screen_name").as[String],
-      (user \ "followers_count").as[Int])
+    def extractUser(userJson: JsValue) = extractUserDefault(userJson, (userJson \ "followers_count").as[Int])
 
-    def extractMentions(mentionsJson: JsValue) = mentionsJson match {
-      case JsArray(elements) => elements.map(extractUser).toSet
-      case _ => Set[User]()
-    }
+    def extractUserDefault(userJson: JsValue, followersDefault: Int) = User(
+      (userJson \ "id_str").as[String],
+      (userJson \ "name").as[String],
+      (userJson \ "screen_name").as[String],
+      followersDefault)
+
+    def extractMentions(mentionsJson: JsValue) = mentionsJson.as[Set[JsValue]].map(extractUserDefault(_, 0))
 
     Tweet(
       (json \ "id_str" ).as[String],
